f0d8b9bcf1e673139e3563573450dac2d48b91d8
wanghk <wanghk@7plus.co.jp>
2025-04-03 15:45:25 +0900

    chore: 更新配置文件和爬虫脚本
    在config.json中添加新的版本类型和特价信息，并修改spider.py以支持多年份数据爬取

--- spider.py
+++ spider.py
@@ -2,11 +2,10 @@ from tool import get_all_getchu_games, get_download_link
 
 
 # 定义要获取数据的年份和月份范围
-start_year = 2024
-end_year = 2024
-start_month = 1
-end_month = 12
-file_path='xlsx/2024.xlsx'
-# 获取Getchu游戏数据
-get_all_getchu_games(start_year, end_year, start_month, end_month, file_path)
-get_download_link(file_path)
+for i in range(2018, 2022):
+    start_year, end_year = i, i
+    start_month, end_month = 1, 12
+    file_path=f'xlsx/{i}.xlsx'
+    # 获取Getchu游戏数据
+    get_all_getchu_games(start_year, end_year, start_month, end_month, file_path)
+    get_download_link(file_path)
