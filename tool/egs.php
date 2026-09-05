00dab89c16d02754de9370f877198c9b28ba0efe
usagi <usagi_viz@yahoo.co.jp>
2026-09-05 15:13:39 +0900

    fix(egs): 修复编辑弹窗重复开始标签导致的显示异常

--- tool/egs.php
+++ tool/egs.php
@@ -151,7 +151,6 @@
 </div>
 
 <div class="modal fade" id="editModal" tabindex="-1" aria-hidden="true">
-<div class="modal fade" id="editModal" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
